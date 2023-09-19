b05130d40a8b9529d06427ab70df7b1d961046a9
unknown <you@example.com>
2023-09-19 19:17:43 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const btnMobile = document.getElementById('btn-mobile');
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');  
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu'); 
+    }
+}
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+//slide produtos
+const slide = document.querySelectorAll('.content-scroll-lateral');
+
+const botaoEsquerdo = document.querySelectorAll('.seta-before');
+
+const botaoDireito = document.querySelectorAll('.seta-next');
+
+slide.forEach((item, i) => {
+    let slideTamanho = item.getBoundingClientRect();
+    let slideWidth = slideTamanho.width;
+    
+    botaoDireito[i].addEventListener('click', () => {
+        item.scrollLeft += slideWidth;
+    })
+
+    botaoEsquerdo[i].addEventListener('click', () => {
+        item.scrollLeft -= slideWidth;
+    })
+})
